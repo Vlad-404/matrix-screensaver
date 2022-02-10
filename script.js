57c0e5c4c7db54d98715f92c91f52bee60760eee
Vlad-404 <vmijat21@gmail.com>
2022-02-10 20:10:30 +0100

    Add insert span with javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Script file has loaded succesfully')
+const container = document.getElementById('container')
+
+// const letters = document.createElement('span')
+// letters.classList.add('letters')
+
+// letters.innerText = 'random characters'
+container.innerHTML = `
+  <span class="letters" style="top: 400px; left: 300px;">Other random chars</span>
+`
+
+console.log(container)
