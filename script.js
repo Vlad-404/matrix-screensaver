d107c0a3913720c2e6682fbda374b9084f537d0b
Vlad-404 <vmijat21@gmail.com>
2022-02-10 21:40:33 +0100

    Add create text every 2 seconds and remove it after 5

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const container = document.getElementById('container')
 
-let randomW = Math.trunc(Math.random() * window.innerWidth);
-let randomH = Math.trunc(Math.random() * window.innerHeight);
-
-console.log(randomW, randomH)
-
 // code snippet from bobbyhadz.com(https://bobbyhadz.com/blog/javascript-set-multiple-attributes-to-element)
 let setAttributes = (element, attributes) => {
   Object.keys(attributes).forEach(attr => {
@@ -14,6 +9,11 @@ let setAttributes = (element, attributes) => {
 // end code snippet
 
 const createSpan = () => {
+  let randomW = Math.trunc(Math.random() * window.innerWidth);
+  let randomH = Math.trunc(Math.random() * window.innerHeight);
+
+  console.log(randomW, randomH)
+
   let textContainer = document.createElement('span')
   textContainer.classList.add('letters')
   console.log(textContainer)
@@ -26,6 +26,10 @@ const createSpan = () => {
   
   container.appendChild(textContainer)
 
+  setTimeout(()=> {
+    textContainer.remove()
+  }, 5000)
+
 }
 
-createSpan()
+setInterval(createSpan, 2000)
